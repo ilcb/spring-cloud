6b3b89ee661f1416423bf90f553c66939f893280
ilcb <ilcbba@126.com>
2017-07-24 18:07:22 +0800

    服务配置中心

---
+++ eureka-server/src/main/java/me/ilcb/ServerApplication.java
@@ -0,0 +1,13 @@
+package me.ilcb;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ServerApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ServerApplication.class).web(true).run(args);
+    }
+}
